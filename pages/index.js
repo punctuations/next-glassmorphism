a9a0ccf75d7c459e05be1500a97801049b1d0c7b
punctuations <matt@dont-ping.me>
2021-01-15 21:51:51 -0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,69 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	useEffect(() => {
+		var c = document.getElementById("canv");
+		var $ = c.getContext("2d");
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+		let col = function (x, y, r, g, b) {
+			$.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+			$.fillRect(x, y, 1, 1);
+		};
+		let R = function (x, y, t) {
+			return Math.floor(192 + 64 * Math.cos((x * x - y * y) / 300 + t));
+		};
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+		let G = function (x, y, t) {
+			return Math.floor(
+				192 +
+					64 *
+						Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300)
+			);
+		};
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+		let B = function (x, y, t) {
+			return Math.floor(
+				192 +
+					64 *
+						Math.sin(
+							5 * Math.sin(t / 9) +
+								((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100
+						)
+			);
+		};
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+		let t = 0;
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+		const run = function () {
+			for (let x = 0; x <= 35; x++) {
+				for (let y = 0; y <= 35; y++) {
+					setTimeout(() => {
+						col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
+					}, 1000);
+				}
+			}
+			t = t + 0.12;
+			window.requestAnimationFrame(run);
+		};
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+		run();
+	}, []);
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+	return (
+		<>
+			<canvas
+				id="canv"
+				width="32"
+				height="32"
+				className="absolute w-screen h-screen"
+			/>
+
+			<main className="w-screen h-screen flex items-center justify-center">
+				<section className="flex glass p-8 m-6 rounded-xl w-1/2 h-1/2 shadow-sm">
+					foo bar
+				</section>
+			</main>
+		</>
+	);
 }
