56ae6877dc0756da20db7ac4b2b64414cc0478dc
punctuations <matt@dont-ping.me>
2021-01-27 21:51:14 -0800

    test overflow hidden on prod bcuz

--- pages/features.js
+++ pages/features.js
@@ -5,7 +5,7 @@ import Content from "../components/assets/content.jsx";
 export default function Features() {
 	return (
 		<>
-			<main className="w-screen h-screen bg-orange-300 bg-image dark:bg-image flex items-center justify-center">
+			<main className="w-screen h-screen bg-orange-300 bg-image dark:bg-image flex items-center justify-center overflow-hidden">
 				<Main>
 					<Nav name="Lorem Ipsum" logo="/favicon.ico" />
 					<Content type="features" />
--- pages/pricing.js
+++ pages/pricing.js
@@ -5,7 +5,7 @@ import Content from "../components/assets/content.jsx";
 export default function Pricing() {
 	return (
 		<>
-			<main className="w-screen h-screen bg-orange-300 bg-image dark:bg-image flex items-center justify-center overflow-y-auto">
+			<main className="w-screen h-screen bg-orange-300 bg-image dark:bg-image flex items-center justify-center overflow-hidden">
 				<Main>
 					<Nav name="Lorem Ipsum" logo="/favicon.ico" />
 					<Content type="pricing" />
