9ab4cd09676e97ab1f28045b5614b32abaf9cf75
punctuations <matt@dont-ping.me>
2021-01-27 21:47:30 -0800

    test on production

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Content from "../components/assets/content.jsx";
 export default function Home() {
 	return (
 		<>
-			<main className="w-screen h-screen bg-orange-400 bg-image dark:bg-image flex items-center justify-center">
+			<main className="w-screen h-screen bg-orange-400 bg-image dark:bg-image flex items-center justify-center overflow-y-auto">
 				<Main>
 					<Nav name="Lorem Ipsum" logo="/favicon.ico" />
 					<Content type="home" />
