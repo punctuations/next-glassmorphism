1814fb694c6b984ca4fe67ed91a2c3b9cf420787
punctuations <matt@dont-ping.me>
2021-01-28 22:59:22 -0800

    seo

--- pages/features.js
+++ pages/features.js
@@ -5,6 +5,34 @@ import Content from "../components/assets/content.jsx";
 export default function Features() {
 	return (
 		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="theme-color" content="#f5bf7c" />
+			</Head>
+			<NextSeo
+				title="features"
+				titleTemplate="%s - glassmorphism"
+				description="An easy way to Innovate"
+				openGraph={{
+					type: "website",
+					url: "https://glassmorphism.vercel.app/features",
+					title: "features - glassmorphism",
+					description: "An easy way to Innovate",
+					images: [
+						{
+							url:
+								"https://www.albertwalicki.com/static/4202381f85c32dc451fd79c4177992c9/hero.png",
+							width: "400px",
+							height: "200px",
+						},
+					],
+				}}
+				twitter={{
+					handle: "@atmattt",
+					site: "@atmattt",
+					cardType: "summary_large_image",
+				}}
+			/>
 			<main className="w-screen h-screen bg-orange-400 dark:bg-black bg-image dark:bg-image-dark flex items-center justify-center transition-all duration-500 overflow-hidden">
 				<Main>
 					<Nav name="Lorem Ipsum" logo="/favicon.ico" />
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,46 @@
+import Head from "next/head";
+import { NextSeo } from "next-seo";
 import Main from "../components/assets/main.jsx";
 import Nav from "../components/assets/nav.jsx";
 import Content from "../components/assets/content.jsx";
 
 export default function Home() {
 	return (
-		<main className="w-screen h-screen bg-orange-400 dark:bg-black bg-image dark:bg-image-dark flex items-center justify-center transition-all duration-500 overflow-hidden">
-			<Main>
-				<Nav name="Lorem Ipsum" logo="/favicon.ico" />
-				<Content type="home" />
-			</Main>
-		</main>
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="theme-color" content="#f5bf7c" />
+			</Head>
+			<NextSeo
+				title="home"
+				titleTemplate="%s - glassmorphism"
+				description="An easy way to Innovate"
+				openGraph={{
+					type: "website",
+					url: "https://glassmorphism.vercel.app/",
+					title: "home - glassmorphism",
+					description: "An easy way to Innovate",
+					images: [
+						{
+							url:
+								"https://www.albertwalicki.com/static/4202381f85c32dc451fd79c4177992c9/hero.png",
+							width: "400px",
+							height: "200px",
+						},
+					],
+				}}
+				twitter={{
+					handle: "@atmattt",
+					site: "@atmattt",
+					cardType: "summary_large_image",
+				}}
+			/>
+			<main className="w-screen h-screen bg-orange-400 dark:bg-black bg-image dark:bg-image-dark flex items-center justify-center transition-all duration-500 overflow-hidden">
+				<Main>
+					<Nav name="Lorem Ipsum" logo="/favicon.ico" />
+					<Content type="home" />
+				</Main>
+			</main>
+		</>
 	);
 }
