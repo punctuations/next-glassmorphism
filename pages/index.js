b41654649f42cbcee69ff26dbf09508b3091fc86
punctuations <matt@dont-ping.me>
2021-01-29 16:17:56 -0800

    z-index

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
 				}}
 			/>
 			<main className="w-screen h-screen bg-orange-400 dark:bg-black bg-image dark:bg-image-dark flex items-center justify-center transition-all duration-500 overflow-hidden">
-				<Main>
+				<Main className="z-50">
 					<Nav name="Lorem Ipsum" logo="/favicon.ico" />
 					<Content type="home" />
 				</Main>
