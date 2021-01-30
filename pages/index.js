384d37c605bf470992675033601104862cdb2876
punctuations <matt@dont-ping.me>
2021-01-29 20:45:51 -0800

    overflow auto

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,8 @@ export default function Home() {
 					cardType: "summary_large_image",
 				}}
 			/>
-			<main className="w-screen h-screen bg-orange-400 dark:bg-black bg-image dark:bg-image-dark flex items-center justify-center transition-all duration-500 overflow-hidden">
-				<Main className="z-50">
+			<main className="w-screen h-screen bg-orange-400 dark:bg-black bg-image dark:bg-image-dark flex items-center justify-center transition-all duration-500 overflow-auto">
+				<Main className="overflow-auto">
 					<Nav name="Lorem Ipsum" logo="/favicon.ico" />
 					<Content type="home" />
 				</Main>
