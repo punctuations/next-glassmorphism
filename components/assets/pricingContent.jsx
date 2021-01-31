56e5812d2adfc465fc1eea82bed610e5bbab7d30
punctuations <matt@dont-ping.me>
2021-01-30 21:47:27 -0800

    pricing

--- components/assets/pricingContent.jsx
+++ components/assets/pricingContent.jsx
@@ -12,22 +12,33 @@ const PricingContent = () => {
 					key={i}
 					className="justify-center flex flex-col flex-grow cursor-default"
 				>
-					<header className="text-3xl my-2">{Tiers.tiers[i].title}</header>
-					<div className="text-lg text-gray-200 my-2">
+					{Tiers.tiers[i].featured ? (
+						<p className="2xl:text-sm xl:text-sm lg:text-sm md:text-xs sm:text-xs text-xs">
+							Featured
+						</p>
+					) : (
+						""
+					)}
+					<header className="2xl:text-3xl xl:text-3xl lg:text-3xl md:text-xl sm:text-xl text-lg 2xl:my-2 xl:my-2 lg:my-2 md:my-2 sm:my-1 my-0.5">
+						{Tiers.tiers[i].title}
+					</header>
+					<div className="2xl:text-lg xl:text-lg lg:text-lg md:text-base sm:text-base text-sm text-gray-200 2xl:my-2 xl:my-2 lg:my-2 md:my-2 sm:my-1 my-0.5">
 						{Tiers.tiers[i].symbol}
-						<span className="text-2xl">
+						<span className="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-xl sm:text-xl text-lg">
 							{Tiers.tiers[i].price} {Tiers.tiers[i].currency}
 						</span>{" "}
-						<span className="text-gray-300 text-sm">
+						<span className="text-gray-300 2xl:text-sm xl:text-sm lg:text-sm md:text-xs sm:text-xs text-xs">
 							{Tiers.tiers[i].interval}
 						</span>
 					</div>
-					<hr className="my-2 border-gray-300" />
+					<hr className="2xl:my-2 xl:my-2 lg:my-2 md:my-2 sm:my-1 my-0.5 border-gray-300" />
 					<div className="flex flex-col items-center">
-						<p className="text-gray-400">{Tiers.tiers[i].description}</p>
+						<p className="text-gray-400 2xl:text-base xl:text-base lg:text-base md:text-sm sm:text-sm text-xs">
+							{Tiers.tiers[i].description}
+						</p>
 					</div>
 					<motion.button
-						className="my-2 p-2 glass dark:glass-dark rounded-lg"
+						className="2xl:text-base xl:text-base lg:text-base md:text-sm sm:text-sm text-xs 2xl:my-2 xl:my-2 lg:my-2 md:my-2 sm:my-1 my-0.5 2xl:p-2 xl:p-2 lg:p-2 md:p-1.5 sm:p-1.5 p-1 glass dark:glass-dark 2xl:rounded-lg xl:rounded-lg lg:rounded-lg md:rounded-md sm:rounded-md rounded-sm"
 						whileHover={{
 							scale: 1.03,
 							transition: {
@@ -50,9 +61,9 @@ const PricingContent = () => {
 						{Tiers.tiers[i].featured ? (
 							<motion.div
 								href="#"
-								className="flex flex-col cursor-default rounded-lg shadow-xl mx-4 p-4 glass dark:glass-dark text-white border-yellow-400"
+								className="rounded-lg shadow-xl 2xl:mx-4 xl:mx-4 lg:mx-4 md:mx-2 sm:mx-1 mx-1 2xl:p-4 xl:p-4 lg:p-4 md:p-3 sm:p-2 p-1.5 glass dark:glass-dark text-white border-yellow-400"
 								key={i}
-								style={{ scale: 1.03 }}
+								style={{ scale: 1.035 }}
 								initial={{
 									y: 20,
 									opacity: 0,
@@ -73,13 +84,12 @@ const PricingContent = () => {
 									},
 								}}
 							>
-								<p>Featured</p>
 								{content}
 							</motion.div>
 						) : (
 							<motion.div
 								href="#"
-								className="rounded-lg shadow-xl mx-4 p-4 glass dark:glass-dark text-white"
+								className="rounded-lg shadow-xl 2xl:mx-4 xl:mx-4 lg:mx-4 md:mx-2 sm:mx-1 mx-1 2xl:p-4 xl:p-4 lg:p-4 md:p-3 sm:p-2 p-2 glass dark:glass-dark text-white"
 								key={i}
 								initial={{
 									y: 20,
