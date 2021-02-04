033ba617b588db319855845c55ba73abf400bdca
punctuations <matt@dont-ping.me>
2021-02-04 15:17:35 -0800

    features padding

--- components/assets/featuresContent.jsx
+++ components/assets/featuresContent.jsx
@@ -12,7 +12,7 @@ const FeaturesContent = () => {
 					key={i}
 					className="justify-center flex flex-col flex-grow cursor-default"
 				>
-					<header className="2xl:text-3xl xl:text-3xl lg:text-3xl md:text-xl sm:text-xl text-lg 2xl:my-2 xl:my-2 lg:my-2 md:my-2 sm:my-1 my-0.5">
+					<header className="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-xl sm:text-xl text-lg 2xl:my-2 xl:my-2 lg:my-2 md:my-2 sm:my-1 my-0.5">
 						{Features.features[i].title}
 					</header>
 					<hr className="my-2 border-gray-300" />
@@ -42,7 +42,7 @@ const FeaturesContent = () => {
 					<>
 						<motion.div
 							href="#"
-							className="rounded-lg shadow-xl 2xl:mx-4 xl:mx-4 lg:mx-4 md:mx-2 sm:mx-1 mx-1 2xl:my-0 xl:my-0 lg:my-0 md:my-2 sm:my-1.5 my-1 2xl:p-4 xl:p-4 lg:p-4 md:p-3 sm:p-2 p-2 glass dark:glass-dark text-white"
+							className="rounded-lg shadow-xl 2xl:mx-4 xl:mx-4 lg:mx-4 md:mx-2 sm:mx-1 mx-1 2xl:my-0 xl:my-0 lg:my-0 md:my-2 sm:my-1.5 my-1 2xl:p-5 xl:p-5 lg:p-5 md:p-4 sm:p-2 p-2 glass dark:glass-dark text-white"
 							key={i}
 							initial={{
 								y: 20,
